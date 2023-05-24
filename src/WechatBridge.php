6b67f8aad5921c5ad3bb7d0ee8f6e16f06def7fb
deloz <npmbot@gmail.com>
2023-05-24 17:47:20 +0800

    feat: Add new payment method to Wechat Bridge

--- src/WechatBridge.php
+++ src/WechatBridge.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Deloz\WechatBridge;
 
 use Deloz\WechatBridge\MiniProgram\Application as MiniProgramApplication;
+use Deloz\WechatBridge\Payment\Application as PaymentApplication;
+use Deloz\WechatBridge\Payment\Payload;
 use EasyWeChat\Kernel\Exceptions\InvalidArgumentException;
 
 final class WechatBridge
@@ -15,5 +17,13 @@ final class WechatBridge
     public static function miniProgram(array $config): MiniProgramApplication
     {
         return new MiniProgramApplication($config);
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    public static function payment(array $config, Payload $payload): PaymentApplication
+    {
+        return new PaymentApplication($config, $payload);
     }
 }
