f2c748c483613da2dcfe8143c184857205cad77d
deloz <npmbot@gmail.com>
2023-06-02 16:58:02 +0800

    feat: add payment notification handlers and serve method to Payment Application class

--- src/Payment/Application.php
+++ src/Payment/Application.php
@@ -6,7 +6,11 @@ namespace Deloz\WechatBridge\Payment;
 
 use Deloz\WechatBridge\Traits\GetApplication;
 use EasyWeChat\Kernel\Exceptions\InvalidArgumentException;
+use EasyWeChat\Kernel\Exceptions\RuntimeException;
 use EasyWeChat\Pay\Application as BaseApplication;
+use Psr\Http\Message\ResponseInterface;
+use ReflectionException;
+use Throwable;
 
 final class Application
 {
@@ -24,5 +28,44 @@ final class Application
     public function getPayment(Payload $payload): Payment
     {
         return new Payment($this->app, $payload);
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     * @throws ReflectionException
+     * @throws RuntimeException
+     * @throws Throwable
+     */
+    public function handlePaidNotify(callable $handler): self
+    {
+        $server = $this->app->getServer();
+        $server->handlePaid($handler);
+
+        return $this;
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     * @throws ReflectionException
+     * @throws RuntimeException
+     * @throws Throwable
+     */
+    public function handleRefundedNotify(callable $handler): self
+    {
+        $server = $this->app->getServer();
+        $server->handleRefunded($handler);
+
+        return $this;
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     * @throws ReflectionException
+     * @throws RuntimeException
+     * @throws Throwable
+     */
+    public function serve(): ResponseInterface
+    {
+        return $this->app->getServer()->serve();
     }
 }
